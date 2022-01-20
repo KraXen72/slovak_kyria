504ccaccc1d9b83bd09805e59446132e396a1dba
KraXen72 <dpelech1@gmail.com>
2022-01-20 21:44:22 +0100

    rules . mk from my vial keymap

--- rules.mk
+++ rules.mk
@@ -1,4 +1,24 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+OLED_ENABLE = no
+#OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_ENCODERS_ENABLE = no
+VIAL_INSECURE = yes
+
+#turn any of these on if needed
+KEY_LOCK_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+GRAVE_ESCAPE_ENABLE = no
+COMBO_ENABLE = no
+TAP_DANCE_ENABLE = no
+AUDIO_ENABLE = no
+
+LTO_ENABLE = yes
+QMK_SETTINGS = no
+NKRO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+DEBOUNCE_TYPE = asym_eager_defer_pk #sym_eager_pk
