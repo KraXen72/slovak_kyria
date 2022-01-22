3041608441cae476feb49f7c7aefb760f580719c
KraXen72 <dpelech1@gmail.com>
2022-01-22 21:48:53 +0100

    add a helper cli script

---
+++ helpercli.py
@@ -0,0 +1,76 @@
+import json
+from textwrap import indent
+
+
+
+print("hello. this is KraXen's VIAL/QMK helper cli")
+
+commands = [
+    "genkey: generate vial customKeycodes from keymap.c"
+]
+
+def find_keycodes(lines):
+    start = -1
+    end = -1
+    for line in lines:
+        if "custom_keycodes" in line:
+            start = lines.index(line)
+            break
+    lines = lines[start:]
+    for line in lines:
+        if "}" in line:
+            end = lines.index(line)
+            break
+    
+    lines = lines[:end+1][1:-1]
+    keycodes = []
+    for line in lines:
+        keycode = line.replace(" ", "")
+        if "=" in line:
+            keycode = keycode.split("=")[0]
+        else:
+            keycode = line.split(",")[0]
+
+        # {"name": "E./", "title": "E ACUTE", "shortName": "E_ACUTE"}
+        name = keycode.replace(" ", "")
+        if "_ACUTE" in name:
+            tempname = name.replace("_ACUTE", "")
+            name = "/\n" + tempname
+        elif "_CARON" in name:
+            tempname = name.replace("_CARON", "")
+            name = "v\n" + tempname
+        
+        formatted = {
+            "name": name.replace(" ", ""),
+            "title": keycode.replace(" ", "").replace("_", " "),
+            "shortName": keycode.replace(" ", "")
+        }
+        keycodes.append(formatted)
+    return keycodes
+
+
+print("\n".join(commands))
+
+command = ""
+while command != "quit":
+    command = input("> ")
+
+    match command:
+        case "genkey": 
+            f = open("keymap.c", 'r', encoding='utf8')
+            lines = f.readlines()
+            f.close()
+
+            keycodes = find_keycodes(lines)
+            print(keycodes)
+            #json_obj = json.dumps(keycodes, indent=4)
+
+            f = open("vial.json", 'r', encoding='utf8')
+            obj = json.load(f)
+            f.close()
+
+            obj["customKeycodes"] = keycodes
+
+            f = open("vial.json", 'w', encoding='utf8')
+            dumped = json.dump(obj, f, indent=4, ensure_ascii=False)
+            f.close()
